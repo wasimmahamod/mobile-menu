e05158e40c80f9912eac1cdc097446063b0b8ad5
wasim <wasim0161719@gmail.com>
2022-09-23 13:35:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let sidbar = document.querySelector('.sidebar')
+let btn = document.querySelector('button')
+let close = document.querySelector('.close')
+let show = false;
+
+
+btn.addEventListener('click',function(){
+    show = !show
+    if(show){
+        sidbar.style.left='-60%';
+    }else{
+        sidbar.style.left = '0%'
+    }
+
+})
+close.addEventListener('click',function(){
+    sidbar.style.left='-20%'
+})
